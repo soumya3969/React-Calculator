6c3e7e8c664625f2db6bc4f453255335ce49f11f
soumya3969 <srmhnt2001@gmail.com>
2024-01-16 23:29:14 +0530

    restructured folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Calculator from './Calculator.js'
+import React from "react";
+import "./App.css";
+import Calculator from "./Calculator.js";
 function App() {
   return (
     <div className="App">
